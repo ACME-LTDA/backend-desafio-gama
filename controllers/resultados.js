c0a48d959e5d5e5ba642563ba7dadcbcff3e0d2a
Diego França <diegobmfranca@gmail.com>
2021-09-01 18:03:35 -0300

    Arrumando a obtenção de dados de resultados de corrida da API

--- controllers/resultados.js
+++ controllers/resultados.js
@@ -4,37 +4,46 @@ const { Resultado } = require('../models/resultados')
 
 
 async function obtemResultadosAno(ano) {
-    const resultados = []
-    await axios.get(`http://ergast.com/api/f1/${ano}/results.json`)
+    let corridas = null
+    await axios.get(`http://ergast.com/api/f1/${ano}/results.json?limit=500`)
         .then(res => {
-            const corridas = res.data['MRData']['RaceTable']['Races']
-            corridas.forEach(({id, idCorrida, idTemporada, idPiloto, idConstrutor, numeroCarro, posLargada, posChegada, voltas, status}) => {
-                const idTemporadaAtual = corrida.season
-                const idCorridaAtual = `${corrida.season}${String(corrida.round).padStart(2, '0')}`
-                const resultadosCorrida = corrida['Results'].map(resultado => {
-                    return resultadoFormatadoModel(resultado, idCorridaAtual, idTemporadaAtual)
-                })
-                resultados.concat(resultadosCorrida)
-            })
+            corridas = res.data['MRData']['RaceTable']['Races']
         });
 
-    return resultados
+    return corridas
+}
+
+function formataListaResultados(corridas, resultados) {
+    let resultadosFormatados = []
+    let resultadosCorrida = null
+    corridas.forEach(({ season, round, Results }) => {
+        const idTemporadaAtual = season;
+        const idCorridaAtual = `${season}${String(round).padStart(2, '0')}`
+        resultadosCorrida = Results.map((resultado) => {
+            return resultadoFormatadoModel(
+                resultado, idCorridaAtual, idTemporadaAtual)
+        })
+        Array.prototype.push.apply(resultadosFormatados, resultadosCorrida)
+    });
+
+    return resultadosFormatados
 }
 
 function resultadoFormatadoModel(resultado, idCorrida, idTemporada) {
-    console.log(resultado)
-    return ({
-        id: `${idCorrida}p${String(resultado.position).padStart(2, '0')}`,
-        idCorrida: idCorrida,
-        idTemporada: idTemporada,
-        idPiloto: resultado.Driver.driverId,
-        idConstrutor: resultado.Constructor.constructorId,
-        numeroCarro: resultado.number,
-        posLargada: resultado.grid,
-        posChegada: resultado.position,
-        voltas: resultado.laps,
-        status: resultado.status,
-    })
+    console.log(`${idCorrida} ${resultado.position}`)
+    if (resultado != undefined)
+        return ({
+            id: `${idCorrida}p${String(resultado.position).padStart(2, '0')}`,
+            idCorrida: idCorrida,
+            idTemporada: idTemporada,
+            idPiloto: resultado.Driver.driverId,
+            idConstrutor: resultado.Constructor.constructorId,
+            numeroCarro: resultado.number,
+            posLargada: resultado.grid,
+            posChegada: resultado.position,
+            voltas: resultado.laps,
+            status: resultado.status,
+        })
 }
 
 async function populaResultadosPeriodo(anoInicio, anoFim) {
@@ -43,10 +52,10 @@ async function populaResultadosPeriodo(anoInicio, anoFim) {
     if(totalResultados == 0) {
         let resultadosAdicionados = 0
         for (let ano = anoInicio; ano <= anoFim; ano++) {
-            const resultados = await obtemResultadosAno(ano)
-            const resultadoFormatado = resultadoFormatadoModel(resultados)
-            await Resultado.create(resultadoFormatado)
-            resultadosAdicionados =+ resultadoFormatado.length
+            const corridas = await obtemResultadosAno(ano)
+            const resultadosFormatados = formataListaResultados(corridas)
+            await Resultado.bulkCreate(resultadosFormatados)
+            resultadosAdicionados += resultadosFormatados.length
         }
         console.log(`${resultadosAdicionados} resultados de corridas de ${anoInicio} a ${anoFim} salvas no banco`)
     }
