eb16f58bb6f7bd693892e8ba6d39bfdecf139399
Diego França <diegobmfranca@gmail.com>
2021-09-07 21:59:26 -0300

    Limpando o index

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const cors = require('cors')
-const cookieParser = require('cookie-parser')
 
 const { iniciaBanco } = require('./models/config-banco')
 const usuariosRoutes = require('./routes/usuarios')
 const sessoesRoutes = require('./routes/sessao')
-const pilotosRoutes = require('./routes/pilotos')
 const uploadRoutes = require('./routes/upload')
-const { obtemSegredo } = require('./controllers/sessao')
 
 require('dotenv').config({ path: __dirname + '/.env' })
 
@@ -15,16 +12,14 @@ const app = express()
 
 const cliente = process.env.CLIENT || 'http://localhost:3000'
 
-app.use(cors({ credentials: true, origin: cliente }))
+app.use(cors())
 app.use(express.static('public'))
-app.use(fileUpload())
+// app.use(fileUpload())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser(obtemSegredo()))
 
 app.use('/usuarios', usuariosRoutes)
 app.use('/sessao', sessoesRoutes)
-app.use('/pilotos', pilotosRoutes)
 app.use('/upload', uploadRoutes)
 
 iniciaBanco()
